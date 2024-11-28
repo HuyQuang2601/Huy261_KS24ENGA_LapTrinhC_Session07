1c2d3c05b5172a85fdf1f092af9a3f26bbac9ca2
HuyQuang2601 <huyxauzai2601@gmail.com>
2024-11-28 13:24:29 +0700

    Add files via upload

---
+++ Session07_Ex10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int n;
+    printf("Nhap so phan tu cua mang:\n ");
+    scanf("%d", &n2);
+    int arr5[n2];
+    printf("Nhap %d phan tu cho mang:\n", n);
+    for (int i = 0; i < n i++) {
+        printf("Nhap phan tu %d: ", i + 1);
+        scanf("%d", &arr5[i]);
+    }
+    for (int i = 0; i < n; i++){
+    		if (arr5[i]==1 || arr5[i]==2){
+    			printf("%d la so nguyen to\n",arr5[i]);
+			}
+    	for (int o = 2; o < arr5[i]; o++){
+    		if (arr5[i] % o == 0){
+    			printf("%d la ko phai so nguyen to\n",arr5[i]);
+    			break;
+			}else{
+				printf("%d la so nguyen to\n",arr5[i]);
+				break;
+			}
+		}
+	}
+    
+    return 0;
+}
